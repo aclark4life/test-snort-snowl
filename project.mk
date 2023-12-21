f0ad670e0177071c15a86a0d1ab7d7a30f4ae15d
Alex Clark <aclark@aclark.net>
2023-12-21 13:14:15 -0500

    Update

--- project.mk
+++ project.mk
@@ -3,3 +3,6 @@ PROJECT_NAME := test-snort-snowl
 install-deps:
 	apt install build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev -y
 
+install-submodules:
+	git submodule init
+	git submodule update
