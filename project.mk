64c1f490cbdbc700095a70bac27b57a9c49870e4
Alex Clark <aclark@aclark.net>
2023-12-21 13:55:14 -0500

    Update

--- project.mk
+++ project.mk
@@ -1,6 +1,6 @@
 PROJECT_NAME := test-snort-snowl
 
-all: install-deps install-submodules install-libdaq install-gperftools install-snort3 install-snowl
+all: install-deps install-submodules install-libdaq install-gperftools install-snort3
 
 install-deps:
 	apt install build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev -y
